4ff9f22b173ab73152915ca412cab697599eac11
Jennifer Wagoner <73571817+jlfarrington@users.noreply.github.com>
2023-06-15 10:16:55 -0500

    update db url

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('postgres://postgres1:3QjnV7Hp8qdZ2JAn66ODA9NJIrlWeIAF@dpg-ci5iiotph6eh6mut9870-a.oregon-postgres.render.com/rainbowconnector',
+const sequelize = new Sequelize('postgres://postgres1:3QjnV7Hp8qdZ2JAn66ODA9NJIrlWeIAF@dpg-ci5iiotph6eh6mut9870-a/rainbowconnector',
                                 'postgres1', 
-                                process.env.DB_PASS);
+                                process.env.DB_PASS, 
+  dialect: 'postgres'
+});
 
 sequelize.authenticate().then(
     function () {
